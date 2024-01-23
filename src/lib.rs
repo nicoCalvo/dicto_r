76832e8d7779b5fc27407be4e670d7ff552430cf
Nico Calvo <salocinov@gmail.com>
2024-01-22 22:02:10 -0300

    feat: search implementation part I

--- src/lib.rs
+++ src/lib.rs
@@ -105,81 +105,112 @@ fn dictor(_py: Python,
     };
     let accumulator: Vec<&PyAny> = vec![];
     let mut inner_item: Result<&PyAny, PyErr>;
-
-    for mut arg in input.args.into_iter(){
-        if inner_object.is_instance_of::<PyDict>(){
-            inner_object = inner_object.downcast::<PyDict>().unwrap();
-        } else if inner_object.is_instance_of::<PyList>(){
-            inner_object = inner_object.downcast::<PyList>().unwrap();
-        } else{
-            if search.is_some(){
-                let py_list = PyList::new(_py, accumulator);
-                return Ok(Some(py_list.to_object(_py)))
-            }else if let Some(default_resp) = default{
-                let resp = PyString::new(_py, default_resp);
-                return Ok(Some(resp.to_object(_py)));
-            }else{
-                return Ok(None)
-            }
-                // si llegue hasta aca y tengo un arg
-                // quiere decir que ya no hay mas por buscar, deberia devolver el acumulador
-                // si search is some sino devolver None o default
-        }
-
-        if ignorecase {
-            // filter by keys
-            // if foudn in key, override key with original value and continue
-            // if not dict, do not do anythin
+    
+    if !input.args.is_empty(){
+        for mut arg in input.args.into_iter(){
             if inner_object.is_instance_of::<PyDict>(){
-                let inner_dict = inner_object.downcast::<PyDict>().unwrap();
-                let cased_key= inner_dict.keys().iter()
-                .filter(|k|{
-                    k.to_string().to_lowercase() == arg.to_lowercase()
-                })
-                .map(|k| k.to_string())
-                .collect::<Vec<String>>();
-                if let Some(key) = cased_key.first(){
-                    arg = key.to_owned();
-                } else{
-                    // TODO: if search is some, return the vec of matches
+                inner_object = inner_object.downcast::<PyDict>().unwrap();
+            } else if inner_object.is_instance_of::<PyList>(){
+                inner_object = inner_object.downcast::<PyList>().unwrap();
+            } else{
+                // if can't keep iterating due to not iterator, then we couldn't find the value
+                // so either default or None value is returned
+                if let Some(default_resp) = default{
+                    let resp = PyString::new(_py, default_resp);
+                    return Ok(Some(resp.to_object(_py)));
+                }else{
+                    return Ok(None)
+                }
 
-                    if let Some(default_resp) = default{
-                        let resp = PyString::new(_py, default_resp);
-                        return Ok(Some(resp.to_object(_py)));
+            if ignorecase {
+                // filter by keys
+                // if found in key, override key with original value and continue
+                // if not dict, do not do anythin
+                if inner_object.is_instance_of::<PyDict>(){
+                    let inner_dict = inner_object.downcast::<PyDict>().unwrap();
+                    let cased_key= inner_dict.keys().iter()
+                    .filter(|k|{
+                        k.to_string().to_lowercase() == arg.to_lowercase()
+                    })
+                    .map(|k| k.to_string())
+                    .collect::<Vec<String>>();
+                    if let Some(key) = cased_key.first(){
+                        arg = key.to_owned();
                     } else{
-                        return Ok(None);
+                        if let Some(default_resp) = default{
+                            let resp = PyString::new(_py, default_resp);
+                            return Ok(Some(resp.to_object(_py)));
+                        } else{
+                            return Ok(None);
+                        }
                     }
                 }
+            };
+            if let Ok(num_arg) = arg.parse::<i32>(){
+                inner_item  = inner_object.get_item(num_arg);
+                if inner_item.is_err(){
+                    inner_item = inner_object.get_item(arg);
+                    // if item matches search, attach to accumulator prior overriding
+                }
             }
-        };
-        if let Ok(num_arg) = arg.parse::<i32>(){
-            inner_item  = inner_object.get_item(num_arg);
-            if inner_item.is_err(){
+            else{
                 inner_item = inner_object.get_item(arg);
-                // if item matches search, attach to accumulator prior overriding
-            }
-        }
-        else{
-            // if item matches search, attach to accumulator prior overriding
-            inner_item = inner_object.get_item(arg);
-        };
-        
-        if let Ok(item) = inner_item{
-            inner_object = item;
+            };
             
-        }else{
-            // TODO: si pasa default usar default
-            if let Some(default_resp) = default{
-                let resp = PyString::new(_py, default_resp);
-                return Ok(Some(resp.to_object(_py)));
+            if let Ok(item) = inner_item{
+
+                inner_object = item;
+                
             }else{
-                return Ok(None);
-            }
-            
-        };
+                if let Some(default_resp) = default{
+                    let resp = PyString::new(_py, default_resp);
+                    return Ok(Some(resp.to_object(_py)));
+                }else{
+                    return Ok(None);
+                }
+                
+            };
+        }
+    }
+    if search.is_some() &&  !inner_object.is_none(){
+        let has_items = true;
+        let search_string = search.unwrap();
+        let search_str = search_string.as_str();
+        println!("yolo");
+
+        // while has_items{
+        //     if inner_object.is_instance_of::<PyDict>(){
+        //         inner_object = inner_object.downcast::<PyDict>().unwrap();
+        //         //por cada key debo iterar de nuevo buscando el valor
+        //         // tiene que tener recursion?
+        //         // buscar keys si existe
+        //         inner_object = inner_object.next();
+        //     } else if inner_object.is_instance_of::<PyList>(){
+        //         inner_object = inner_object.downcast::<PyList>().unwrap();
+        //         let res =  inner_object.iter()?.map(|element|{
+        //             if inner_object.is_instance_of::<PyDict>(){
+        //                 let elm = inner_object.get_item(search_str);
+        //                 match elm {
+        //                     Ok(v) => accumulator.push(v),
+        //                     Err(_)=>println!("value not found")
+        //                 };
+        //             }
+        //         });
+        //     } else if inner_object.valu
+
+
+        }
+
+        for elm in inner_object.iter()?{
+            if inner_object.is_instance_of::<PyDict>(){
+                inner_object = inner_object.downcast::<PyDict>().unwrap();
+            } else if inner_object.is_instance_of::<PyList>(){
+                inner_object = inner_object.downcast::<PyList>().unwrap();
+            } 
+        }
+        // loopear en inner_dict por cada elemento ver si existe el valor para una key
     }
     //TODO checknone: if None is found and `checknone`` set a PyErr must be raised
-    // no puede devolver anidaciones, solo flat lists?
     if inner_object.is_none() && default.is_some(){
         let default_resp = PyString::new(_py, default.unwrap());
         Ok(Some(default_resp.to_object(_py)))
@@ -189,6 +220,37 @@ fn dictor(_py: Python,
     
 }
 
+
+fn find_occurences(py: Python, target: &str, searchable: &PyAny, accumulator: &PyList){
+    dbg!(searchable);
+    if searchable.is_instance_of::<PyList>(){
+        let iter = searchable.iter().unwrap();
+        for maybe_element in iter {
+            if let Ok(element) = maybe_element{
+                find_occurences(py, &target, element, accumulator);
+               
+            }
+        }
+    }else if searchable.is_instance_of::<PyDict>(){
+        
+        let inner_dict = searchable.downcast::<PyDict>().unwrap();
+        for key in inner_dict.keys(){
+            if let Some(matching_item) = inner_dict.get_item(key){
+                if matching_item.is_instance_of::<PyDict>() ||
+                matching_item.is_instance_of::<PyList>(){
+                    find_occurences(py, target, matching_item, accumulator)
+                }else{
+                    if matching_item.is_instance_of::<PyString>() && key.to_string() == target{
+                        let string = matching_item.downcast::<PyString>().unwrap();
+                        accumulator.append(string);
+                    } 
+                }
+
+            }
+        }
+    }
+
+}
 
 #[pymodule]
 pub fn dicto_r(_py: Python, m: &PyModule) -> PyResult<()> {
@@ -203,6 +265,39 @@ mod tests {
     use pyo3::Python;
 
     use super::*;
+
+    #[test]
+    fn find_occurences_test(){
+        // ejemplo facil:
+        // lista de jsons, busco un valor:
+        pyo3::prepare_freethreaded_python();
+        Python::with_gil(|py| {
+            let my_string = PyString::new(py, "pepe");
+            let content = my_string.to_str();
+            let mut elements: Vec<&PyDict> = vec![];
+            
+            for elm in ["pepe", "pipo", "popo"].into_iter(){
+                let elm1 = PyDict::new(py);
+                elm1.set_item("name", elm);
+                elm1.set_item("last_name", format!("{elm}_last_name"));
+                elements.push(elm1);
+            }
+            let elm1 = PyDict::new(py);
+            elm1.set_item("name", "papa");
+            let elements2 : Vec<&PyDict> = vec![elm1];
+            let elm2 = PyDict::new(py);
+            elm2.set_item("pepe", elements2);
+            elements.push(elm2);
+            let vec_accumulator : Vec<PyString>= vec![];
+            let base_list = PyList::new(py, elements);
+            let accumulator = PyList::new(py, vec_accumulator);
+            find_occurences(py, "name", &base_list, accumulator);
+            dbg!(accumulator);
+        });
+        
+    }
+
+
 
     #[test]
     fn test_int_as_string(){
